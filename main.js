f47bc4d3c151a14e8979dbdcc34bc5282320eb79
Chikage04 <104400056+Chikage04@users.noreply.github.com>
2022-06-17 01:27:34 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+const door = document.getElementsByClassName('hcb-icon hcb-door-out')[0];
+const door2 = document.getElementsByClassName('hcb-icon hcb-door-in')[0];
+const aide = document.getElementsByClassName('gintama')[0];
+if (door.style.display !== 'none') {
+    aide.style.display = 'none';
+}
+
