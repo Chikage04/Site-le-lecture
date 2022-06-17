5d3af9e1ec7e0017e99c4ab256eda27691b0046e
Chikage04 <104400056+Chikage04@users.noreply.github.com>
2022-06-18 01:21:17 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -3,9 +3,12 @@
 setInterval(() => {
     const door = document.querySelector('#HCB_comment_box #hcb_form .hcb-door-out')
     const aide = document.getElementsByClassName('gintama')[0];
-
+    const help = document.getElementsById('ghcb_msg')[0]; 
     if (door.style.display !== 'none') {
         aide.style.display = 'none';
+    }
+    if(help.style.display !== 'none'){
+        aide.style.top = '34vh';
     }
 }, 500);
 
