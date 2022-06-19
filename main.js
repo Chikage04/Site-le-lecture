fe772aec42b8355495fdb555b42f3f67440dbb05
Chikage04 <104400056+Chikage04@users.noreply.github.com>
2022-06-20 00:13:43 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 
 
+
 setInterval(() => {
     const door = document.querySelector('#HCB_comment_box #hcb_form .hcb-door-out')
     const aide = document.getElementsByClassName('gintama')[0];
@@ -13,12 +14,12 @@ setInterval(() => {
 }, 500);
 
 setInterval(() => {
+    const Lscreen = screen.width;
     const aide = document.getElementsByClassName('gintama')[0];
     const help = document.querySelector('#hcb_msg')
 
-
-    aide.style.top = help && help.textContent ? '34vh' : '31vh';
-
+    aide.style.top = help && help.textContent && Lscreen < "1090" ? '37vh' : '30vh';
+    aide.style.top = help && help.textContent && Lscreen > "1900" ? '34vh' : '31vh';
 
 
 }, 100);
