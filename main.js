45e5e2c1739c7099139ab8c3cca762d5713dc918
Chikage04 <104400056+Chikage04@users.noreply.github.com>
2022-07-10 15:37:47 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -17,6 +17,8 @@ setInterval(() => {
     const Lscreen = screen.width;
     const aide = document.getElementsByClassName('gintama')[0];
     const help = document.querySelector('#hcb_msg')
+    const flag = document.querySelector('.hcb-flag')
+    const flag2 = document.getElementsByClassName('hcb-flag');
     const repl = document.querySelector("#HCB_comment_box #hcb_form #replying_to_container")
 
     if (help && help.textContent) {
@@ -28,6 +30,12 @@ setInterval(() => {
     }
     if (help && help.textContent) {
         help.textContent = 'Connectez-vous pour commenter'
+    }
+    if (flag && flag.textContent) {
+        flag.textContent = 'Report'
+    }
+    if (flag2 && flag2.textContent) {
+        flag2.textContent = 'Report'
     }
     if (repl && repl.textContent) {
         if (screen.width < 1090) {
